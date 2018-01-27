7a37083bf653ecf81cdcd7fa87f67cad1b9595e3
Lin Mulan <admin@snake1999.com>
2018-01-27 17:17:14 +0800

    Unit testing for Binary: Separating and benchmarking

--- binary/Binary_test.go
+++ binary/Binary_test.go
@@ -5,59 +5,24 @@ import (
 	"math"
 )
 
-func TestBinary(t *testing.T) {
+var correct =
+	"0100ABCDFF637FFF8000FFFF00787FFFFFFFF1DA38F47FFFFFFFFFFFFFFF8000000000000000FFFFFFFFFFFF" +
+		"FFFF000000003ADE68B140490FDB7F7FFFFF4005BF0A8B1457697FEFFFFFFFFFFFFFFF7FC7CFFFFFD204FFFF" +
+		"FF7F85FFFFFFFFFFFFFFFFFFFF7FEB81CE64CDE633FBFFFFFFFFFFFFFFFF7FC9120000000000DB0F4940FFFF" +
+		"7F7F6957148B0ABF0540FFFFFFFFFFFFEF7F0000000A676F2072616B206C696201E24000000140E201010000"
+
+func TestBinaryWrite(t *testing.T) {
 	buf := new([]byte)
 
-	correct := "0100ABCDFF637FFF8000FFFF00787FFFFFFFF1DA38F47FFFFFFFFFFFFFFF8000000000000000FFFFFFFFFFFF" +
-			"FFFF000000003ADE68B140490FDB7F7FFFFF4005BF0A8B1457697FEFFFFFFFFFFFFFFF7FC7CFFFFFD204FFFF" +
-			"FF7F85FFFFFFFFFFFFFFFFFFFF7FEB81CE64CDE633FBFFFFFFFFFFFFFFFF7FC9120000000000DB0F4940FFFF" +
-			"7F7F6957148B0ABF0540FFFFFFFFFFFFEF7F0000000A676F2072616B206C696201E24000000140E201010000"
+	doWriteTest(buf)
 
-	WriteBool					(buf, true)
-	WriteBool					(buf, false)
-	WriteByte					(buf, 0xab)
-	WriteByte					(buf, 0xcd)
-	WriteUnsignedByte			(buf, 255)
-	WriteUnsignedByte			(buf, 99)
-	WriteShort					(buf, math.MaxInt16)
-	WriteShort					(buf, -32768)
-	WriteUnsignedShort			(buf, math.MaxUint16)
-	WriteUnsignedShort			(buf, 120)
-	WriteInt					(buf, math.MaxInt32)
-	WriteInt					(buf, -237356812)
-	WriteLong					(buf, math.MaxInt64)
-	WriteLong					(buf, -9223372036854775808)
-	WriteUnsignedLong			(buf, math.MaxUint64)
-	WriteUnsignedLong			(buf, 987654321)
-	WriteFloat					(buf, math.Pi)
-	WriteFloat					(buf, math.MaxFloat32)
-	WriteDouble					(buf, math.E)
-	WriteDouble					(buf, math.MaxFloat64)
-	WriteLittleShort			(buf, math.MaxInt16)
-	WriteLittleShort			(buf, -12345)
-	WriteLittleUnsignedShort	(buf, math.MaxUint16)
-	WriteLittleUnsignedShort	(buf, 1234)
-	WriteLittleInt				(buf, math.MaxInt32)
-	WriteLittleInt				(buf, -123)
-	WriteLittleLong				(buf, math.MaxInt64)
-	WriteLittleLong				(buf, -345678976543456789)
-	WriteLittleUnsignedLong		(buf, math.MaxUint64)
-	WriteLittleUnsignedLong		(buf, 1231231)
-	WriteLittleFloat			(buf, math.Pi)
-	WriteLittleFloat			(buf, math.MaxFloat32)
-	WriteLittleDouble			(buf, math.E)
-	WriteLittleDouble			(buf, math.MaxFloat64)
-	WriteString					(buf, "")
-	WriteString					(buf, "go rak lib")
-	WriteBigEndianTriad			(buf, 123456)
-	WriteBigEndianTriad			(buf, 0x1)
-	WriteLittleEndianTriad		(buf, 123456)
-	WriteLittleEndianTriad		(buf, 0x1)
-
-	if bytesToStr(buf) != correct {
+	if bytesToHex(buf) != correct {
 		t.Error("Incorrect buffer writing")
 	}
+}
 
+func TestBinaryRead(t *testing.T) {
+	buf := hexToBytes(correct)
 
 	off := 0
 	if ReadBool					(buf, &off) != true { t.Error("Incorrect buffer reading: ReadBool")}
@@ -103,13 +68,77 @@ func TestBinary(t *testing.T) {
 
 }
 
+func BenchmarkWrite(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		buf := new([]byte)
+		doWriteTest(buf)
+	}
+}
 
-func bytesToStr(b *[]byte) string {
+func doWriteTest(buf *[]byte) {
+	WriteBool					(buf, true)
+	WriteBool					(buf, false)
+	WriteByte					(buf, 0xab)
+	WriteByte					(buf, 0xcd)
+	WriteUnsignedByte			(buf, 255)
+	WriteUnsignedByte			(buf, 99)
+	WriteShort					(buf, math.MaxInt16)
+	WriteShort					(buf, -32768)
+	WriteUnsignedShort			(buf, math.MaxUint16)
+	WriteUnsignedShort			(buf, 120)
+	WriteInt					(buf, math.MaxInt32)
+	WriteInt					(buf, -237356812)
+	WriteLong					(buf, math.MaxInt64)
+	WriteLong					(buf, -9223372036854775808)
+	WriteUnsignedLong			(buf, math.MaxUint64)
+	WriteUnsignedLong			(buf, 987654321)
+	WriteFloat					(buf, math.Pi)
+	WriteFloat					(buf, math.MaxFloat32)
+	WriteDouble					(buf, math.E)
+	WriteDouble					(buf, math.MaxFloat64)
+	WriteLittleShort			(buf, math.MaxInt16)
+	WriteLittleShort			(buf, -12345)
+	WriteLittleUnsignedShort	(buf, math.MaxUint16)
+	WriteLittleUnsignedShort	(buf, 1234)
+	WriteLittleInt				(buf, math.MaxInt32)
+	WriteLittleInt				(buf, -123)
+	WriteLittleLong				(buf, math.MaxInt64)
+	WriteLittleLong				(buf, -345678976543456789)
+	WriteLittleUnsignedLong		(buf, math.MaxUint64)
+	WriteLittleUnsignedLong		(buf, 1231231)
+	WriteLittleFloat			(buf, math.Pi)
+	WriteLittleFloat			(buf, math.MaxFloat32)
+	WriteLittleDouble			(buf, math.E)
+	WriteLittleDouble			(buf, math.MaxFloat64)
+	WriteString					(buf, "")
+	WriteString					(buf, "go rak lib")
+	WriteBigEndianTriad			(buf, 123456)
+	WriteBigEndianTriad			(buf, 0x1)
+	WriteLittleEndianTriad		(buf, 123456)
+	WriteLittleEndianTriad		(buf, 0x1)
+}
+
+var hexCode = []string{"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"}
+var hexMap = map[uint8] byte {
+	'0': 0x0,'1': 0x1,'2': 0x2,'3': 0x3,
+	'4': 0x4,'5': 0x5,'6': 0x6,'7': 0x7,
+	'8': 0x8,'9': 0x9,'A': 0xA,'B': 0xB,
+	'C': 0xC,'D': 0xD,'E': 0xE,'F': 0xF,
+}
+
+func bytesToHex(b *[]byte) string {
 	ans := ""
-	code := []string{"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"}
 	for i := 0; i < len(*b); i++ {
-		ans += code[((*b)[i]>>4)&0x0f]
-		ans += code[((*b)[i])&0x0f]
+		ans += hexCode[((*b)[i]>>4)&0x0f]
+		ans += hexCode[((*b)[i])&0x0f]
 	}
 	return ans
+}
+
+func hexToBytes(s string) *[]byte {
+	ans := *new([]byte)
+	for i := 0; i < len(s); i+=2 {
+		ans = append(ans, hexMap[s[i]] << 4 + hexMap[s[i+1]])
+	}
+	return &ans
 }
