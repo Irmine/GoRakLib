64f24220a87bfbc390a926d7627efbd0180e0e6f
Sandertv <st.ten.veldhuis@gmail.com>
2018-03-05 20:46:51 +0100

    Removed useless for loop in test.

--- server/server_test.go
+++ server/server_test.go
@@ -12,6 +12,7 @@ func Test(t *testing.T) {
 	manager := NewManager()
 	manager.Start("0.0.0.0", 19132)
 	manager.PongData = "MCPE;This is the MOTD;201;1.2.10.2;0;20;" + strconv.Itoa(int(manager.ServerId)) + ";GoMine;Creative;"
+
 	manager.PacketFunction = func(packet []byte, session *Session) {
 		fmt.Println("Packet:", hex.EncodeToString(packet[0:1]))
 	}
@@ -21,8 +22,7 @@ func Test(t *testing.T) {
 	manager.DisconnectFunction = func(session *Session) {
 		fmt.Println(session, "disconnected!")
 	}
-	for manager.Running {
-		time.Sleep(time.Minute)
-		manager.Stop()
-	}
+
+	time.Sleep(time.Minute)
+	manager.Stop()
 }
